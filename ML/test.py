0b7ef3d6aa085ef6dda8ad357afb68b3349f0e81
Zhangweiguo <zhangweiguo717@163.com>
2017-09-21 23:41:49 +0800

    tensorflow

---
+++ ML/test.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+import numpy
+# data = datasets.load_diabetes()
+# print data.__class__
+# x,y = data['data'],data['target']
+# print x.__class__,y.__class__
+# print x,y
+# print x.shape, y.shape
+
+print('hello world')
+x = 0
+for i in range(10):
+    print i
+    x += 1
+
+print numpy.random.rand(3, 3)
